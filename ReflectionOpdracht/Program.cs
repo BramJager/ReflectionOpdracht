77c38450c204d03c661efeef472fd87653d860d6
Bram Jager <bramjager@upcmail.nl>
2021-12-16 14:07:08 +0100

    Added BusinessLayer

--- ReflectionOpdracht/Program.cs
+++ ReflectionOpdracht/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ReflectionOpdracth.Business;
+using System;
 using System.Reflection;
 
 namespace ReflectionOpdracht
@@ -7,65 +8,32 @@ namespace ReflectionOpdracht
     {
         static void Main(string[] args)
         {
-            Assembly assembly = Assembly.LoadFile(@"C:\Users\Bram Jager\Desktop\ReflectThis.dll");
-            var list = assembly.GetTypes();
-            foreach (var item in list)
-            {
-                Console.WriteLine(item);
-                Console.WriteLine("\tMethods");
-                foreach (var method in item.GetMethods())
-                {
-                    Console.WriteLine($"\t\t{method}");
-                }
-                Console.WriteLine("\tFields");
-                foreach (var field in item.GetFields())
-                {
-                    Console.WriteLine($"\t\t{field}");
-                }
-                Console.WriteLine("\tProperty");
-                foreach (var property in item.GetProperties())
-                {
-                    Console.WriteLine($"\t\t{property}");
-                }
-                Console.WriteLine("\tInterfaces");
-                foreach (var interfaceInfo in item.GetInterfaces())
-                {
-                    Console.WriteLine($"\t\t{interfaceInfo}");
-                }
-                Console.WriteLine();
-            }
-
-            var userType = assembly.GetType("ReflectThis.User");
-            var userInstance = Activator.CreateInstance(userType);
-
-            var setUsername = userType.GetMethod("set_Username");
-            var getUsername = userType.GetMethod("get_Username");
-            var setPassword = userType.GetMethod("set_Password");
-            var getPassword = userType.GetMethod("get_Password");
+            string assemblyPath = @"C:\Users\Bram Jager\Desktop\ReflectThis.dll";
 
             string username = "Henk";
             string password = "password";
 
-            Console.WriteLine($"Set Username to: {username}");
-            setUsername.Invoke(userInstance, new object[] { username });
-            Console.WriteLine($"Set Password to: {password}");
-            setPassword.Invoke(userInstance, new object[] { password });
+            UserAssemblyService service = new UserAssemblyService();
 
-            Console.WriteLine($"Get Username: {getUsername.Invoke(userInstance, null)}");
-            Console.WriteLine($"Get Password: {getPassword.Invoke(userInstance, null)}");
+            service.WriteAssemblyInfoToConsole(assemblyPath);
 
-            var userManagerType = assembly.GetType("ReflectThis.UserManager");
-            var userManagerInstance = Activator.CreateInstance(userManagerType);
+            var assembly = service.GetAssembly(assemblyPath);
 
-            var getUsercount = userManagerType.GetMethod("get_UserCount");
-            var LogOn = userManagerType.GetMethod("Logon");
-            var logOf = userManagerType.GetMethod("LogOff");
+            var managerInstance = service.CreateUserManagerInstance(assembly);
 
-            Console.WriteLine($"Logged in with {username} & {password}: {LogOn.Invoke(userManagerInstance, new object[] {username, password})}");
-            Console.WriteLine($"Number of Users: {getUsercount.Invoke(userManagerInstance, null)}");
-            Console.WriteLine($"Logged off: {username}");
-            logOf.Invoke(userManagerInstance, new object[] { username });
-            Console.WriteLine($"Number of Users: {getUsercount.Invoke(userManagerInstance, null)}");
+            service.LogInUser(username, password, managerInstance, assembly);
+            service.WriteNumberOfLoggedInUsers(assembly, managerInstance);
+            Console.WriteLine();
+
+            service.LogInUser("username", "password", managerInstance, assembly);
+            service.WriteNumberOfLoggedInUsers(assembly, managerInstance);
+            Console.WriteLine();
+
+            service.LogOfUser(username, assembly, managerInstance);
+            service.WriteNumberOfLoggedInUsers(assembly, managerInstance);
+            Console.WriteLine();
+
+            service.WriteLogInTestSequenceToConsole(username, password, assembly);
 
         }
     }
---
+++ ReflectionOpdracth.Business/UserAssemblyService.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectionOpdracth.Business
+{
+    public class UserAssemblyService
+    {
+        public void WriteAssemblyInfoToConsole(string assemblyPath)
+        {
+            Assembly assembly = Assembly.LoadFile(assemblyPath);
+            var list = assembly.GetTypes();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine("\tMethods");
+                foreach (var method in item.GetMethods())
+                {
+                    Console.WriteLine($"\t\t{method}");
+                }
+                Console.WriteLine("\tFields");
+                foreach (var field in item.GetFields())
+                {
+                    Console.WriteLine($"\t\t{field}");
+                }
+                Console.WriteLine("\tProperty");
+                foreach (var property in item.GetProperties())
+                {
+                    Console.WriteLine($"\t\t{property}");
+                }
+                Console.WriteLine("\tInterfaces");
+                foreach (var interfaceInfo in item.GetInterfaces())
+                {
+                    Console.WriteLine($"\t\t{interfaceInfo}");
+                }
+                Console.WriteLine();
+            }
+        }
+        public Assembly GetAssembly(string assemblyPath)
+        {
+            return Assembly.LoadFile(assemblyPath);
+        }
+
+        public object CreateUserInstance(Assembly assembly)
+        {
+            var userType = assembly.GetType("ReflectThis.User");
+            return Activator.CreateInstance(userType);
+        }
+
+        public object CreateUserManagerInstance(Assembly assembly)
+        {
+            var userManagerType = assembly.GetType("ReflectThis.UserManager");
+            return Activator.CreateInstance(userManagerType);
+        }
+
+        public void LogInUser(string username, string password, object managerInstance, Assembly assembly)
+        {
+            var userType = assembly.GetType("ReflectThis.User");
+            var userInstance = Activator.CreateInstance(userType);
+
+            var setUsername = userType.GetMethod("set_Username");
+            var setPassword = userType.GetMethod("set_Password");
+
+            Console.WriteLine($"Set Username to: {username}");
+            setUsername.Invoke(userInstance, new object[] {username});
+            Console.WriteLine($"Set Password to: {password}");
+            setPassword.Invoke(userInstance, new object[] {password});
+
+            var userManagerType = assembly.GetType("ReflectThis.UserManager");
+
+            var LogOn = userManagerType.GetMethod("Logon");
+            Console.WriteLine($"Logged in with {username} & {password}: {LogOn.Invoke(managerInstance, new object[] { username, password })}");
+        }
+
+        public void LogOfUser(string username, Assembly assembly, object managerInstance)
+        {
+            var userManagerType = assembly.GetType("ReflectThis.UserManager");
+
+            var logOf = userManagerType.GetMethod("LogOff");
+            Console.WriteLine($"Logged off {username}: {logOf.Invoke(managerInstance, new object[] { username })}");
+        }
+
+        public int GetNumberOfLoggedInUsers(Assembly assembly, object managerInstance)
+        {
+            var userManagerType = assembly.GetType("ReflectThis.UserManager");
+
+            var getUsercount = userManagerType.GetMethod("get_UserCount");
+
+            return (int)getUsercount.Invoke(managerInstance, null);
+        }
+
+        public void WriteNumberOfLoggedInUsers(Assembly assembly, object managerInstance)
+        {
+            Console.WriteLine($"Number of logged in users: {GetNumberOfLoggedInUsers(assembly, managerInstance)}");
+        }
+
+        public void WriteLogInTestSequenceToConsole(string username, string password, Assembly assembly)
+        {
+            var userType = assembly.GetType("ReflectThis.User");
+            var userInstance = Activator.CreateInstance(userType);
+
+            var setUsername = userType.GetMethod("set_Username");
+            var getUsername = userType.GetMethod("get_Username");
+            var setPassword = userType.GetMethod("set_Password");
+            var getPassword = userType.GetMethod("get_Password");
+
+
+            Console.WriteLine($"Set Username to: {username}");
+            setUsername.Invoke(userInstance, new object[] { username });
+            Console.WriteLine($"Set Password to: {password}");
+            setPassword.Invoke(userInstance, new object[] { password });
+
+            Console.WriteLine($"Get Username: {getUsername.Invoke(userInstance, null)}");
+            Console.WriteLine($"Get Password: {getPassword.Invoke(userInstance, null)}");
+
+            var userManagerType = assembly.GetType("ReflectThis.UserManager");
+            var userManagerInstance = Activator.CreateInstance(userManagerType);
+
+            var getUsercount = userManagerType.GetMethod("get_UserCount");
+            var LogOn = userManagerType.GetMethod("Logon");
+            var logOf = userManagerType.GetMethod("LogOff");
+
+            Console.WriteLine($"Logged in with {username} & {password}: {LogOn.Invoke(userManagerInstance, new object[] { username, password })}");
+            Console.WriteLine($"Number of Users: {getUsercount.Invoke(userManagerInstance, null)}");
+            Console.WriteLine($"Logged off {username}: {logOf.Invoke(userManagerInstance, new object[] { username })}");
+            Console.WriteLine($"Number of Users: {getUsercount.Invoke(userManagerInstance, null)}");
+        }
+    }
+}
